b9a1206b90ac9ecffb7414dc70c39d6e81b9a3b4
Ilya <ilyalebedev1@yandex.ru>
2018-04-07 04:51:43 +0300

    CRUD 4 completed

---
+++ app/models/racer.rb
@@ -0,0 +1,46 @@
+class Racer
+
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+  include Mongoid::Document
+
+  def id
+  	@id.to_s
+  end
+
+  def mongo_client
+    Mongoid::Clients.default
+  end
+
+  def initialize(params={})
+  	super(params)
+  	@id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+  	@number=params[:number].to_i
+  	@first_name=params[:first_name]
+  	@last_name=params[:last_name]
+  	@gender=params[:gender]
+  	@group=params[:group]
+  	@secs=params[:secs].to_i
+  end
+
+  def collection
+    self.mongo_client['racers']
+  end
+
+  def self.all(prototype={}, sort={:number => 1}, skip=0, limit=nil)
+   result = collection.find(prototype)        
+   .projection({_id:true, number:true, first_name:true, last_name:true, gender:true, group:true, secs:true})        
+   .sort(sort)        
+   .skip(skip)      
+
+   result = result.limit(limit) if !limit.nil?      
+   return result  
+  end
+
+  def self.find id
+  	id = id.to_s
+  	result = collection.find({:_id => BSON::ObjectId(id)}).first
+  	return result.nil? ? nil : Racer.new(result)
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Raceday
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    Mongoid.load!('./config/mongoid.yml')
+    config.generators {|g| g.orm :mongoid}
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
