63f846772ede5fbe191cf5c80dc0f4e9fae832b3
Ilya <ilyalebedev1@yandex.ru>
2018-04-10 21:30:18 +0300

    no active record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+#require "active_resource/railtie" # Comment this line for Rails 4.0+
+require "rails/test_unit/railtie"
+require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +30,6 @@ module Raceday
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
